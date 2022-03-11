67c80ef62685c86e218ff63538be4b7c4327b7e6
yisy0703 <yisy0703@naver.com>
2022-03-11 17:04:12 +0900

    03-11

--- source/01_java/ch03_operator/src/com/lec/quiz/Quiz1.java
+++ source/01_java/ch03_operator/src/com/lec/quiz/Quiz1.java
@@ -8,5 +8,6 @@ public class Quiz1 {
 		int su = scanner.nextInt(); // 사용자로부터 정수 입력 받기
 		String result = su%3 == 0 ? "입력하신 수는 3의 배수" : "입력하신 수는 3의 배수가 아님";
 		System.out.println(result);
+		scanner.close();
 	}
 }
