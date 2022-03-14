f8c5b53d40167faf28e18c176b6c0decf296eab9
yisy0703 <yisy0703@naver.com>
2022-03-14 16:10:57 +0900

    03-14

--- source/01_java/ch04_control/src/com/lec/loop/Quiz3for.java
+++ source/01_java/ch04_control/src/com/lec/loop/Quiz3for.java
@@ -1,16 +1,10 @@
 package com.lec.loop;
-import java.util.Scanner;
-//	사용자로부터 원하는 단수의 구구단을 출력하자
+//	5단 구구단을 출력하자
 public class Quiz3for {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("몇단 원해?");
-//		int dansu = scanner.nextInt();
-		int dansu = 5;
 		for(int i=1 ; i<10 ; i++) {
-			//System.out.printf("%d * %d = %d\n", dansu, i, dansu*i);
-			System.out.println(dansu+"*"+i+"="+(dansu*i));
+			System.out.println("5 * "+ i + " = " + 5*i);
+//			System.out.printf("5 * %d = %d\n", i, 5*i);
 		}
-		scanner.close();
 	}
 }
