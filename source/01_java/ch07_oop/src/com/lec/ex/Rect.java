41f571320bb8be866e5d18b0fcdc39b43f14c822
yisy0703 <yisy0703@naver.com>
2022-03-16 16:14:49 +0900

    03-16

--- source/01_java/ch07_oop/src/com/lec/ex/Rect.java
+++ source/01_java/ch07_oop/src/com/lec/ex/Rect.java
@@ -7,5 +7,29 @@ public class Rect {
 	public int area() {
 		return width*height;
 	}
-	// setter & getter 만들어 보기
+	// setter
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	// getter
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
