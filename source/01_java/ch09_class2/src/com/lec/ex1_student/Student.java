afce68a3b003e7b41fcb2e75c19c4dcf50539675
yisy0703 <yisy0703@naver.com>
2022-03-17 17:50:17 +0900

    03-17

---
+++ source/01_java/ch09_class2/src/com/lec/ex1_student/Student.java
@@ -0,0 +1,33 @@
+package com.lec.ex1_student;
+
+public class Student {
+	private String name;
+	private int kor;
+	private int eng;
+	private int mat;
+	private int tot;
+	private double avg;
+	// Student s = new Student("沥快己",90,90,90);
+	public Student(String name, int kor, int eng, int mat) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+		tot = kor + eng + mat;
+		avg = tot / 3.0;
+	}
+	// s.print() -> 沥快己 90 90 90 270 90.0 免仿
+	public void print() {
+		System.out.printf("\t %s \t %d \t %d \t %d \t %d \t %.1f\n", name, kor, eng, mat, tot, avg);
+	}
+	// System.out.println(s.infoString());
+	public String infoString() {
+		return String.format("\t %s \t %d \t %d \t %d \t %d \t %.1f\n", name, kor, eng, mat, tot, avg);
+	}
+	public String getName() {return name;}
+	public int getKor() {return kor;}
+	public int getEng() {return eng;}
+	public int getMat() {return mat;}
+	public int getTot() {return tot;}
+	public double getAvg() {return avg;}
+}
