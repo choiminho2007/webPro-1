26fbbd3d23314739dee7c244ee9d5ae06e9d0696
yisy0703 <yisy0703@naver.com>
2022-03-18 14:17:05 +0900

    03-18

---
+++ source/01_java/ch09_class2/src/com/lec/ex5_momchild/Child.java
@@ -0,0 +1,35 @@
+package com.lec.ex5_momchild;
+// Child first = new Child("첫째");
+// first.takeMoney(1000); 엄마 천원만
+public class Child {
+	private String name;
+	MomPouch momPouch;
+	public Child() {}
+	public Child(String name) {
+		this.name = name;
+		momPouch = new MomPouch();
+	}
+	public void takeMoney(int money) {
+		if(momPouch.money >= money) {
+//			momPouch.money = momPouch.money - money;
+			momPouch.money -= money;
+			System.out.println(name+"가 "+money+"원 가져가서 엄마 지갑에 "+
+							momPouch.money+"원 있음");
+		}else {
+			System.out.println(name+"가 돈을 못 받음. 현재 엄마 지갑에는 "+
+							momPouch.money +"원밖에 없음");		
+		}//if
+	}//takeMoney
+}//main
+
+
+
+
+
+
+
+
+
+
+
+
