01a79e76a280cb3c4e08cc83926e8056d3a2b77c
yisy0703 <yisy0703@naver.com>
2022-03-21 14:37:47 +0900

    03-21

---
+++ source/01_java/ch10_inheritance/src/com/lec/ex06_override/TestMain.java
@@ -0,0 +1,39 @@
+package com.lec.ex06_override;
+public class TestMain {
+	public static void main(String[] args) {
+		ParentClass pObj1 = new ParentClass();
+		ParentClass pObj2 = new ParentClass(1);
+		if(pObj1.equals(pObj2)) {
+			System.out.println("두 객체는 같다");
+		}else {
+			System.out.println("두 객체는 다르다");
+		}
+		ChildClass cObj = new ChildClass(2);
+		if(cObj.equals(pObj2)) {
+			System.out.println("무조건 true지");
+		}
+		pObj1.method1();
+		pObj1.method2();
+		System.out.println("------------------");
+		cObj.method1(); // ChildClass의 method1
+		cObj.method2(); // ParentClass의 method2
+		cObj.method3(); // ChildClass의 method3
+		ParentClass cObj1 = new ChildClass(2);
+		cObj1.method1(); // ChildClass의 method1
+		cObj1.method2(); // ParentClass의 method2
+//		cObj1.method3(); 불가-변수의 type이 ChildClass가 아닌 ParentClass라
+		// ParentClass형 cObj1변수를 ChildClass형으로 변환가능하니?
+		if (cObj1 instanceof ChildClass) {
+			((ChildClass)cObj1).method3();
+		}else {
+			System.out.println("형변환 안 됨");
+		}
+	}//main
+}//class
+
+
+
+
+
+
+
