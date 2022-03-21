05cba4f7fc251da978de72bc52a0c7bc44a360bd
yisy0703 <yisy0703@naver.com>
2022-03-21 15:22:41 +0900

    03-22

---
+++ source/01_java/ch10_inheritance/src/com/lec/ex08_super/ChildClass.java
@@ -0,0 +1,28 @@
+package com.lec.ex08_super;
+//super.  : 내 부모단의
+//super() : 내 부모단의 생성자 함수	
+public class ChildClass extends ParentClass{ // ParentClass - i, method()
+	private int i = 99;
+	public ChildClass() {
+		System.out.println("ChildClass 생성자");
+	}
+	@Override
+	public void method() {
+		System.out.println("ChildClass의 method");
+		super.method();
+		System.out.println("ChildClass에서 super의 i="+super.getI()+", Child단의 i="+i);
+	}
+	@Override
+	public int getI() {return i;}
+}
+
+
+
+
+
+
+
+
+
+
+
