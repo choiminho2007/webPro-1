9baf2a99918fb5f63fe5c6d8f7eeea3f2c797fe8
yisy0703 <yisy0703@naver.com>
2022-03-22 10:57:05 +0900

    03-22

---
+++ source/01_java/ch10_inheritance/src/com/lec/ex12_store/Store3.java
@@ -0,0 +1,23 @@
+package com.lec.ex12_store;
+//Áõ±Ç°¡¿¡ ¸ÅÀå3È£Á¡: ±èÄ¡Âî°³-6,000  ºÎ´ëÂî°³-7,000  ºñºö¹ä-7,000 ¼ø´ë±¹-6,000
+public class Store3 extends HeadQuarterStore{
+	public Store3(String str) {
+		super(str);
+	}
+	@Override
+	public void kimchi() {
+		System.out.println("±èÄ¡Âî°³ - 6,000¿ø");
+	}
+	@Override
+	public void bude() {
+		System.out.println("ºÎ´ëÂî°³ - 7,000¿ø");
+	}
+	@Override
+	public void bibib() {
+		System.out.println("ºñºö¹ä - 7,000¿ø");
+	}
+	@Override
+	public void sunde() {
+		System.out.println("¼ø´ë±¹ - 6,000¿ø");
+	}
+}
---
+++ source/01_java/ch10_inheritance/src/com/lec/ex12_store/TestMain.java
@@ -0,0 +1,26 @@
+package com.lec.ex12_store;
+public class TestMain {
+	public static void main(String[] args) {
+		HeadQuarterStore[] store  = {new HeadQuarterStore("= = = 본사 = = ="), 
+									new Store1("= = = 1호점 = = ="),
+									new Store2("= = = 2호점 = = ="),
+									new Store3("= = = 3호점 = = =")};
+//		store[4] = new Store2("다른 2호점");
+		for(int idx=0 ; idx<store.length ; idx++) {
+			System.out.println(store[idx].getStr());
+			store[idx].kimchi();
+			store[idx].bude();
+			store[idx].bibib();
+			store[idx].sunde();
+			store[idx].gonggibab();
+		}
+		for(HeadQuarterStore st : store) {
+			System.out.println(st.getStr());
+			st.kimchi();
+			st.bude();
+			st.bibib();
+			st.sunde();
+			st.gonggibab();
+		}
+	}
+}
