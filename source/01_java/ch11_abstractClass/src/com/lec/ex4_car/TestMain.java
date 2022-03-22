f5e9321497e78cef3051db313379430ab93aa114
yisy0703 <yisy0703@naver.com>
2022-03-22 17:55:10 +0900

    03-22

--- source/01_java/ch11_abstractClass/src/com/lec/ex4_car/TestMain.java
+++ source/01_java/ch11_abstractClass/src/com/lec/ex4_car/TestMain.java
@@ -1,7 +1,10 @@
 package com.lec.ex4_car;
-
+import com.lec.cons.CarSpec;
 public class TestMain {
 	public static void main(String[] args) {
-		Car car = new LowGradeCar("회색", "일반", 1000, "일반");
+//		Car car = new LowGradeCar("회색", "일반", 1000, "일반");
+		Car car = new LowGradeCar(CarSpec.GRAY_CAR, CarSpec.TIRE_NORMAL, 
+							CarSpec.DISPLACEMENT_1000, CarSpec.HANDDLE_POWER);
+		car.getSpec();
 	}
 }
