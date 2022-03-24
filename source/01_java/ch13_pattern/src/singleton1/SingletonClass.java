052f9e00a84ec974cf621f95508394e30c5f04b1
yisy0703 <yisy0703@naver.com>
2022-03-24 15:25:37 +0900

    03-24

--- source/01_java/ch13_pattern/src/singleton1/SingletonClass.java
+++ source/01_java/ch13_pattern/src/singleton1/SingletonClass.java
@@ -3,11 +3,8 @@ public class SingletonClass {
 	private int i;
 	// 객체생성했으면 그 객체 주소를 return하고 
 	// 객체생성 안 했으면 객체생성하고 그 주소를 return
-	private static SingletonClass INSTANCE;
+	private static SingletonClass INSTANCE = new SingletonClass();
 	public static SingletonClass getInstane() {
-		if(INSTANCE==null) {
-			INSTANCE = new SingletonClass();
-		}
 		return INSTANCE;
 	}
 	private SingletonClass() {}
--- source/01_java/ch13_pattern/src/singleton1/TestMain.java
+++ source/01_java/ch13_pattern/src/singleton1/TestMain.java
@@ -1,9 +1,14 @@
 package singleton1;
 public class TestMain {
 	public static void main(String[] args) {
-		SingletonClass obj1 = SingletonClass.getInstane();
-		SingletonClass obj2 = SingletonClass.getInstane();
+		SingletonClass obj1 
+			= SingletonClass.getInstane();
+		SingletonClass obj2 
+			= SingletonClass.getInstane();
 		obj1.setI(99);
+		System.out.println("obj1의 i : "+obj1.getI());
+		System.out.println("obj2의 i : "+obj2.getI());
+		obj2.setI(10);
 		System.out.println("obj1의 i : "+obj1.getI());
 		System.out.println("obj2의 i : "+obj2.getI());
 	}
