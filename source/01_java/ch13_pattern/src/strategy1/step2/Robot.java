d5eaabf3680975f2e13310222980257bb1f7e78f
yisy0703 <yisy0703@naver.com>
2022-03-24 16:47:21 +0900

    03-24

--- source/01_java/ch13_pattern/src/strategy1/step2/Robot.java
+++ source/01_java/ch13_pattern/src/strategy1/step2/Robot.java
@@ -11,7 +11,7 @@ public class Robot {
 		String className = getClass().getName(); // strategy1.step2.SuperRobot
 		int idx = className.lastIndexOf('.'); // 맨 마지막 '.'이 있는 위치 
 		String name = className.substring(idx+1); // idx+1번째 글씨부터 끝까지
-		System.out.println("입니다. 외형은 팔, 다리, 몸통, 머리가 있습니다.");
+		System.out.println(name + "입니다. 외형은 팔, 다리, 몸통, 머리가 있습니다.");
 	}
 }
 
