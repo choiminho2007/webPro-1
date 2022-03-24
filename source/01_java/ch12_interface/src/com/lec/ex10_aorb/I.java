12a80e983eb0e4725c9617d9a0744ebf288fb5f5
yisy0703 <yisy0703@naver.com>
2022-03-24 13:45:45 +0900

    03-24

---
+++ source/01_java/ch12_interface/src/com/lec/ex10_aorb/I.java
@@ -0,0 +1,4 @@
+package com.lec.ex10_aorb;
+public interface I {
+	public void method();
+}
---
+++ source/01_java/ch12_interface/src/com/lec/ex10_aorb/User.java
@@ -0,0 +1,6 @@
+package com.lec.ex10_aorb;
+public class User {
+	public void use(I i) {
+		i.method();
+	}
+}
