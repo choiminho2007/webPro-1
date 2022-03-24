8c5007bddf8b41781f8de5d4f3f99e26607295b0
yisy0703 <yisy0703@naver.com>
2022-03-24 18:06:03 +0900

    03-24

--- source/01_java/ch13_pattern/src/strategy1/step4/modularization/TestMain.java
+++ source/01_java/ch13_pattern/src/strategy1/step4/modularization/TestMain.java
@@ -6,6 +6,6 @@ public class TestMain {
 	public static void main(String[] args) {
 		Robot robot = new SuperRobot();
 		robot.actionFly();
-//		robot.setFly(new FlyNo());  5단계
+//		robot.setFly(new FlyNo());  5단계 외부에서 업그레이드 가능하도록
 	}
 }
