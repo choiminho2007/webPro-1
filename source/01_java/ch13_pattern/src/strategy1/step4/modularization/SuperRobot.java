f5750abbf5fb8cf634996c5c5ff6ed3142955fc4
yisy0703 <yisy0703@naver.com>
2022-03-24 17:57:40 +0900

    03-24

--- source/01_java/ch13_pattern/src/strategy1/step4/modularization/SuperRobot.java
+++ source/01_java/ch13_pattern/src/strategy1/step4/modularization/SuperRobot.java
@@ -1,5 +1,10 @@
 package strategy1.step4.modularization;
-import strategy1.step4.interfaces.*;
+import strategy1.step4.interfaces.FlyYes;
+import strategy1.step4.interfaces.IFly;
+import strategy1.step4.interfaces.IKnife;
+import strategy1.step4.interfaces.IMissile;
+import strategy1.step4.interfaces.KnifeLazer;
+import strategy1.step4.interfaces.MissileYes;
 public class SuperRobot extends Robot {
 	private IFly     fly;
 	private IMissile missile;
---
+++ source/01_java/ch13_pattern/src/strategy1/step4/modularization/TestMain.java
@@ -0,0 +1,11 @@
+package strategy1.step4.modularization;
+
+import strategy1.step4.interfaces.FlyNo;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Robot robot = new SuperRobot();
+		robot.actionFly();
+//		robot.setFly(new FlyNo());  5´Ü°è
+	}
+}
