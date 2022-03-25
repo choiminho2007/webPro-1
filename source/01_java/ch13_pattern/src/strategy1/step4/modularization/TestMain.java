77043ccdefbee367bcde0306633a80102058b83f
yisy0703 <yisy0703@naver.com>
2022-03-25 09:15:09 +0900

    03-24

--- source/01_java/ch13_pattern/src/strategy1/step4/modularization/TestMain.java
+++ source/01_java/ch13_pattern/src/strategy1/step4/modularization/TestMain.java
@@ -1,7 +1,5 @@
 package strategy1.step4.modularization;
 
-import strategy1.step4.interfaces.FlyNo;
-
 public class TestMain {
 	public static void main(String[] args) {
 		Robot robot = new SuperRobot();
